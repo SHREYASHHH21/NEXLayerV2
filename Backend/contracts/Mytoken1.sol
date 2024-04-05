8b3364c78ca61340d03f3de5456f325477b9bec3
manan9993 <mananbhagat9993@gmail.com>
2024-04-06 00:40:08 +0530

    burn function added to  Mytoken1.sol

--- Backend/contracts/Mytoken1.sol
+++ Backend/contracts/Mytoken1.sol
@@ -8,4 +8,7 @@ contract Mytoken1 is ERC20 {
      function mint(address to, uint256 amount) external {
         _mint(to, amount);
     }
+     function burn(address to,uint256 amount) external {
+        _burn(to, amount);
+    }
 }
