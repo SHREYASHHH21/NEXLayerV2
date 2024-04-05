0f1012ea6ffa214de305a8a31a5379bc76211229
@Yash9276 <yashkudnar519@gmail.com>
2024-04-05 23:40:35 +0530

    adding contracts

---
+++ Backend/contracts/Mytoken.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract Mytoken is ERC20 {
+    constructor() ERC20("xEthereum", "xETH") {
+    }
+     function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+     function burn(address to,uint256 amount) external {
+        _burn(to, amount);
+    }
+}
